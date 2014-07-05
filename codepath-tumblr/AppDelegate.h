3d7029479ee6f228ddbc2d5939342ecf30934e2c
Mario Delgado <mario.c.delgado@gmail.com>
2014-07-05 15:23:53 -0700

    Initial Commit

---
+++ codepath-tumblr/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  codepath-tumblr
+//
+//  Created by Mario C. Delgado Jr. on 7/5/14.
+//  Copyright (c) 2014 Mario C. Delgado Jr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ codepath-tumblr/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  codepath-tumblr
+//
+//  Created by Mario C. Delgado Jr. on 7/5/14.
+//  Copyright (c) 2014 Mario C. Delgado Jr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
